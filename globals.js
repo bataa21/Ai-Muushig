b73e7c67eee396857403329a28f68aaa9f0fcfe0
bataa21 <76891522+bataa21@users.noreply.github.com>
2025-05-23 10:20:12 +0800

    Add files via upload

--- globals.js
+++ globals.js
@@ -1,14 +1,11 @@
-// globals.js
-const suits = ['♠', '♥', '♦', '♣'];
-const ranks = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const suits = ['C', 'D', 'H', 'S']; // Clubs, Diamonds, Hearts, Spades
+const ranks = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A']; // Muushig 32-card deck
 
 let deck = [];
-let trumpCard = null;
-
 let playerHand = [];
 let botHand = [];
-
+let trumpCard = '';
 let playerScore = 15;
 let botScore = 15;
-
 let isPlayerTurn = true;
+
